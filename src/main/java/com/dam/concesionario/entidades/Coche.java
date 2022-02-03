dbbc5d75826463116c7d096674d2b17a663ac883
Manuel Torres <manueltorres.informatica@gmail.com>
2022-02-03 09:59:21 +0100

    Añadir interfaz Serializable a la Entidad Coche

--- src/main/java/com/dam/concesionario/entidades/Coche.java
+++ src/main/java/com/dam/concesionario/entidades/Coche.java
@@ -1,5 +1,7 @@
 package com.dam.concesionario.entidades;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +12,7 @@ import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "dam_coches")
-class Coche {
+class Coche implements Serializable{
 
 	/** SERIAL ID */
 	private static final long serialVersionUID = 1L;
