3343a465e9a610d19030ca6a1fa48333a3e6c119
Manuel Torres <manueltorres.informatica@gmail.com>
2022-02-03 11:38:07 +0100

    Añadido fichero script import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO dam_coches (Matricula, Marca, Modelo) VALUES('4762-BKC', 'Renault', 'Megane');
+INSERT INTO dam_coches (Matricula, Marca, Modelo) VALUES('6732-LMV', 'Mercedez', 'Benz');
+INSERT INTO dam_coches (Matricula, Marca, Modelo) VALUES('9281-KCB', 'Volkswagen', 'Golf');
+INSERT INTO dam_coches (Matricula, Marca, Modelo) VALUES('0098-GVB', 'Seat', 'León');
+INSERT INTO dam_coches (Matricula, Marca, Modelo) VALUES('8376-FVZ', 'Ford', 'Focus');
+INSERT INTO dam_coches (Matricula, Marca, Modelo) VALUES('5491-LDN', 'KIA', 'Rio');
+INSERT INTO dam_coches (Matricula, Marca, Modelo) VALUES('3265-DES', 'BMW', 'X1');
+INSERT INTO dam_coches (Matricula, Marca, Modelo) VALUES('1986-HBU', 'Citroen', 'Jumper');
