4c3cb47e9443f67d4eca884886632e670fd78b2f
Manuel Torres <manueltorres.informatica@gmail.com>
2022-02-07 23:53:36 +0100

    Añadida clase implementada del servicio

--- src/main/java/com/dam/concesionario/servicios/CocheServiceI.java
+++ src/main/java/com/dam/concesionario/servicios/CocheServiceI.java
@@ -1,5 +1,22 @@
 package com.dam.concesionario.servicios;
 
+import java.util.List;
+
+import com.dam.concesionario.entidades.Coche;
+
 public interface CocheServiceI {
 
+	public List<Coche> obtenerTodosCoches();
+
+	public Coche obtenerCochePorMatricula(final String Matricula);
+
+	public List<Coche> obtenerCochePorMarcaOModelo(final String Marca, final String Modelo);
+
+	public List<Coche> obtenerCochePorMarcaYModelo(final String Marca, final String Modelo);
+
+	public void eliminarCochePorId(final long IdCoche);
+
+	public void aniadirCoche(final Coche coche);
+
+	public void actualizarCoche(final Coche coche);
 }
---
+++ src/main/java/com/dam/concesionario/servicios/CocheServiceImpl.java
@@ -0,0 +1,53 @@
+package com.dam.concesionario.servicios;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.dam.concesionario.entidades.Coche;
+import com.dam.concesionario.repositorio.CocheRepositorioI;
+
+public class CocheServiceImpl implements CocheServiceI{
+
+	@Autowired
+	private CocheRepositorioI cocheRepositorio;
+	
+	@Override
+	public List<Coche> obtenerTodosCoches() {
+		return cocheRepositorio.findAll();
+	}
+
+	@Override
+	public Coche obtenerCochePorMatricula(String matricula) {
+		final Coche coche = (Coche) cocheRepositorio.findByMatricula(matricula);
+		return coche;
+	}
+
+	@Override
+	public List<Coche> obtenerCochePorMarcaOModelo(String Marca, String Modelo) {
+		return cocheRepositorio.findByMarcaOrModelo(Marca, Modelo);
+	}
+
+	@Override
+	public List<Coche> obtenerCochePorMarcaYModelo(String Marca, String Modelo) {
+		return cocheRepositorio.findByMarcaAndModelo(Marca, Modelo);
+	}
+
+	@Override
+	public void eliminarCochePorId(long IdCoche) {
+		cocheRepositorio.deleteById(IdCoche);	
+	}
+
+	@Override
+	public void aniadirCoche(Coche coche) {
+		cocheRepositorio.save(coche);	
+	}
+
+	@Override
+	public void actualizarCoche(Coche coche) {
+		cocheRepositorio.save(coche);
+	}
+	
+	
+
+}
